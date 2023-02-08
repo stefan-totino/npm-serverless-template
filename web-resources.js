4ba9921368d3dd990117864fa5709fff6489bb1c
stef totino <stefantotino@gmail.com>
2023-02-08 02:43:38 -0500

    more generic and smaller

--- web-resources.js
+++ web-resources.js
@@ -1,13 +1,10 @@
-const serverless = require('serverless-http');
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const [serverless, express] = [require('serverless-http'), require('express')];
+const [app, bodyParser] = [express(), require('body-parser')];
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/index.html', async (req, res) => {
-    await res.send("<h1>Hello World! From an AWS Lambda function provided by ZECTR :)</h1>")
+    await res.send("<h1>Hello World! From an AWS Lambda function via GitHub Actions :)</h1>")
 })
 
 module.exports.handler = serverless(app);
- 
